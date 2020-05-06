281334a7bb1d882759049982c95657e236e47cb6
Robert M. Nill <rmnill202mail@gmail.com>
2020-05-06 18:22:24 -0400

    Configured for pre-rendering

--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes
+  mode: 'history',
+  routes,
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,31 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');1
+
 module.exports = {
-  publicPath: '/2020-portfolio-site/',
+  // publicPath: '/2020-portfolio-site/',
+  // outputDir: `dist/2020-portfolio-site/`,
+
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/2020-portfolio-site/'
+    : '/',
+  outputDir: process.env.NODE_ENV === 'production' 
+    ? path.join(__dirname, 'dist/2020-portfolio-site/')
+    : path.join(__dirname, 'dist/'),
+
+  pluginOptions: {
+    prerenderSpa: {
+      staticDir: path.join(__dirname, 'dist'),
+      outputDir: path.join(__dirname, 'dist/2020-portfolio-site'),
+      indexPath: path.join(__dirname, 'dist', '/2020-portfolio-site/index.html'),
+      registry: undefined,
+      renderRoutes: [
+        '/',
+        '/project/1',
+        '/project/2'
+      ],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 }
