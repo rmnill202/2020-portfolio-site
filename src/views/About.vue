f004412bf6ada22f81ae92b3ba660372f2b92b49
Robert M. Nill <rmnill202mail@gmail.com>
2022-09-25 16:33:34 -0400

    About updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,11 +3,35 @@
     <div class="about-background"></div>
     <div class="about-page">
       <div class="display-2 about-title font-weight-thin">About Me</div>
-      <div class="about-section font-weight-light">Studied Software Engineering (BS) and Computer Science (MS) at RIT between 2015 and 2020. </div>
-      <div class="about-section font-weight-light">Passionate about web development, software architecture/design and graphics programming. </div>
-      <div class="about-section font-weight-light">My hobbies outside of software development include music performance, movies and cooking. Maybe all at the same time.</div>
+      <!-- <div class="about-section font-weight-light">Studied Software Engineering (BS) and Computer Science (MS) at RIT between 2015 and 2020. </div> -->
+      <!-- <div class="about-section font-weight-light">Passionate about web development, software architecture/design and graphics programming. </div>
+      <div class="about-section font-weight-light">My hobbies outside of software development include music performance, movies and cooking. Maybe all at the same time.</div> -->
 
-      <div class="link-section" v-for="site in info.links"><a :href="site.link">{{site.title}}</a></div>
+      <div class="about-section font-weight-light">I studied Software Engineering (BS) and Computer Science (MS) at RIT between 2015 and 2020, taking a few software co-ops during my time around the Northeastern US. </div>
+
+      <div class="about-section font-weight-light">Outside of work, I love to tinker! I've got a couple 3D printers, a workbench made out of a 95% off IKEA kitchen door, and am self-taught in FreeCAD!</div>
+
+      <div class="about-section font-weight-light">You can check out some of my projects on Twitter, such as:
+        <ul> 
+          <li>LED control system for lighting around my apartment</li>
+          <li>Designing small form factor PCs</li>
+          <li>Making a very janky sandblaster for some prototype PC parts</li>
+          <li>Building PCs (you may notice a pattern here)</li>
+          <li>Custom cable making for PCs (there's definitely a pattern)</li>
+        </ul>
+      </div>
+
+      <div class="about-section font-weight-light">In my downtime I love to play games with friends, go disc golfing, or rock climb at my local gym.</div>
+
+
+
+      <!-- <div class="link-section" v-for="site in info.links"><a :href="site.link">{{site.title}}</a></div> -->
+
+      <div class="link-section">
+        <a v-for="site in info.links" :key="site.link" class="link-block" :href="site.link">
+          <img :src="require(`@/assets/${site.icon}`)" />
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -25,7 +49,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .about-page {
   position: relative;
   text-align: left;
@@ -36,7 +60,7 @@ export default {
 }
 
 .about-section {
-  margin-bottom: 10px;
+  margin-bottom: 36px;
 }
 
 .about-section-2 {
@@ -46,7 +70,12 @@ export default {
 }
 
 .link-section {
+  margin-top: 200px;
   text-align: center;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  /* max-width: 1200px; */
 }
 
 .link-section a {
@@ -76,5 +105,35 @@ export default {
   background-size: 1022px;
 }
 
+.link-block {
+  /* background-color: rgb(255, 255, 255); */
+  width: 120px;
+  height: 120px;
+  margin: 16px;
+  /* :src="require(`@/assets/2018-portfolio/wireframes.png`) */
+  /* --link-block-icon: '~@/assets/icons/linkedin.svg'; */
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 8px;
+  background-color: #1f032725;
+
+  transition: all 0.25s cubic-bezier(.72,.01,.19,.94);
+}
+.link-block:hover {
+  /* background-color: rgba(255, 255, 255, 0.288); */
+  background-color: #37094483;
+}
+.link-block img {
+  width: 100%;
+  height: 100%;
+  transform-origin: center;
+  transform: scale(90%);
+  transition: transform 0.55s cubic-bezier(.72,.01,.19,.94);
+}
+.link-block:hover img {
+  transform: scale(100%);
+}
 </style>
